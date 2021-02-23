77bb04eedb22f93dcf9d3a3757d3ccb09fbadc13
yaros.vodotyka@gmail.com <yaros.vodotyka@gmail.com>
2021-02-23 19:24:14 +0200

    add db context to startup

--- Artem.Doctors.Api/Startup.cs
+++ Artem.Doctors.Api/Startup.cs
@@ -1,6 +1,9 @@
+using Artem.Doctors.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +15,25 @@ namespace Artem.Doctors.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        private readonly IHostEnvironment _environment;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">Configuration of the web API.</param>
+        /// <param name="environment">Hosting environment.</param>
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
+        {
+            _configuration = configuration;
+            _environment = environment;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DoctorsDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("DoctorsDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
